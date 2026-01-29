db2b16c5ce8bf438cb74f0885daf48553bb87602
Djordje Miladinovic <miladinovic.djordje@gmail.com>
2026-01-29 09:40:49 +0100

    remove pre-footer section

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata, Viewport } from "next";
 import { Playfair_Display, Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
-import PreFooter from "@/components/pre-footer";
 import Footer from "@/components/footer";
 import StickyButtons from "@/components/sticky-buttons";
 import { CANONICAL_BASE } from "@/lib/constants";
@@ -91,7 +90,6 @@ export default function RootLayout({
       <body className="antialiased flex min-h-screen flex-col">
         <Header />
         <main className="flex-1">{children}</main>
-        <PreFooter />
         <Footer />
         <StickyButtons />
         <SanityLive />
